bf29fc723beb13a7cd735f8bb605bb4f5200f363
DKD07 <161266358+DKD07@users.noreply.github.com>
2024-03-01 22:51:51 +0530

    Add files via upload

---
+++ Takinguserinput.py
@@ -0,0 +1,11 @@
+a=input("Enter your name: ")
+print("My name is :",a)
+#Here,ini BELOW, only INPUT take STRING not an INTEGER
+x=input("Enter the first number:")
+y=input("Enter the second number:")
+print(x+y)
+#HERE,in BELOW,we take INT input and taking the integer input
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+print(x+y)
+
